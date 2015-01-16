664c162d8c49b2fc2da2c9f66cc7bbe82d7dad70
WienWong <wangzichengfu@gmail.com>
2015-01-16 12:04:09 +0100

    Update SI_Proj1.R
    2nd v

--- SI_Proj1.R
+++ SI_Proj1.R
@@ -1,5 +1,4 @@
-
-# Statistical Inference Course Project
+### Project part 1
 
 # Create an empty vector to store all of our sample means. 
 lambda = 0.2
@@ -9,13 +8,11 @@ myexpd <- rep(NA, 1000)
 # Simulate the distribution of the mean of 40 exponentials. Set lambda = 0.2 for all of the simulations.
 
 set.seed(512)
+
 for (i in 1:1000){
     tmp <- rexp(n, lambda)
     myexpd[i] <- mean(tmp)  # 40 samples of exponential distribution rexp(n, lambda) 
 }
-
-# Histogram plot
-hist(myexpd)
 
 # Mean
 myexpd_m <- mean(myexpd)
@@ -33,44 +30,25 @@ mean_t <- 1 / lambda
 var_t <- (1/lambda)^2/n
 # 0.625
 
-# scale(myexpd)
-
-hist(myexpd, probability=T, main="", xlab="")
-lines(density(myexpd))
-
-# Compare with the standard normal distribution
-curve(dnorm(x,myexpd_m, sqrt(myexpd_v)), 2, 8, col="red", add=T)
-
-# Convert to data frame for ggplot
+# Convert to data frame for ggplot.
 df <- data.frame(myexpd)
-# head(df)
-# dim(df)
+
+head(df)
+dim(df)
 
 library(ggplot2)
 
-g <- ggplot(data = df, aes(x = myexpd)) 
+g <- ggplot(data=df, aes(x=myexpd)) 
 
-g + geom_histogram(aes(y=..density..), fill = I('blue'), binwidth = 0.20, color = I('black')) + stat_function(fun = dnorm, arg = list(mean = mean_t, sd = sqrt(var_t)), color = "red")
+g + geom_histogram(aes(y=..density..), fill=I('blue'), binwidth=0.40, color=I('black')) + stat_function(fun=dnorm, arg =list(mean=mean_t, sd=sqrt(var_t)), color="red") + geom_vline(xintercept=mean_t, colour="green", linetype="longdash")
 
-# the coverage of the confidence interval for 1/lambda
-lowercl <- myexpd - qnorm(0.975) * (1/lambda)/sqrt(n)
-uppercl <- myexpd + qnorm(0.975) * (1/lambda)/sqrt(n)
-expci <- mean(lowercl < (1/0.2) & uppercl > (1/0.2))
-expci
-#The confidence interval is thus expci : 95.3%
+# use qqplot and qqline to compare the distribution of averages of 40 exponentials to a normal distribution
+qqnorm(myexpd)
 
-# Evaluate the coverage of the confidence interval for 1/lambda
+qqline(myexpd, col = 2)
+
+# Evaluate the 95% confidence interval for this simulation:
 mean(df$myexpd) + c(-1,1)*1.96*sd(df$myexpd)/sqrt(nrow(df))
 # 4.928790 5.026791
-
-
-
-
-
-
-
-
-
-
 
 
